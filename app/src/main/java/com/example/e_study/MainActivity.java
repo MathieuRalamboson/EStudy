9d9e98bc9bb6aea392683e2b83c2160413c2bace
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-05-08 15:59:21 +0200

    Commit 1.3 :
    1/Erreur acces Class User
    2/Erreur sur librairie

--- app/src/main/java/com/example/e_study/MainActivity.java
+++ app/src/main/java/com/example/e_study/MainActivity.java
@@ -1,13 +1,142 @@
 package com.example.e_study;
 
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText NewUser,NewPassword,NewEmail;// For sign up
+    MaterialEditText User,Password;//For sign in
+
+    Button btnSignUp,btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        User = (MaterialEditText) findViewById(R.id.User);
+        Password = (MaterialEditText)findViewById(R.id.Password);
+
+        btnSignIn = (Button)findViewById(R.id.btn_sign_in);
+        btnSignUp = (Button)findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vieww) {
+                signIn(User.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+
+    private void signIn(final String user, final String pwd) {
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(user).exists())
+                {
+                    if(!user.isEmpty())
+                    {
+                        User login = dataSnapshot.child(user).getValue(User.class);
+                        if(login.getPassword().equals(pwd))
+                            Toast.makeText(MainActivity.this,"Login ok", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Wrong password", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,"Please enter your user name", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                    Toast.makeText(MainActivity.this,"User is not exists", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showSignUpDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View sign_up_layout = inflater.inflate(R.layout.sign_up_layout,null);
+
+        NewUser = (MaterialEditText)sign_up_layout.findViewById(R.id.NewUserName);
+        NewEmail = (MaterialEditText)sign_up_layout.findViewById(R.id.NewEmail);
+        NewPassword = (MaterialEditText)sign_up_layout.findViewById(R.id.NewPassword);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                final User user = new User(NewUser.getText().toString(),
+                        NewPassword.getText().toString(),
+                        NewEmail.getText().toString());
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(user.getUserName()).exists())
+                            Toast.makeText(MainActivity.this,"User already exist !", Toast.LENGTH_SHORT).show();
+                        else{
+                            users.child(user.getUserName())
+                                    .setValue(user);
+                            Toast.makeText(MainActivity.this,"User registration success !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
