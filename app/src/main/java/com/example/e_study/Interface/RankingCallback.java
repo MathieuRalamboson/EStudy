45fc125f4845d3bbb69d13b91e05ae66bfb64a7f
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-05-29 00:36:12 +0200

    Commit 2.0 :
    1/ Affichage Sign up/in opérationel
    2/Choix entre 2 Catégories
    3/Page départ de quizz créé et ajouté
    4/Page avec Ranking user créé mais encore vide
    5/Page avec Quizz / Reponse /Boutton créé mais non fonctionnel
    6/Page de fin de Quizz , avec récap du score et Try again créé
    
    7/Page ranking user : problème de récupération de données

---
+++ app/src/main/java/com/example/e_study/Interface/RankingCallback.java
@@ -0,0 +1,5 @@
+package com.example.e_study.Interface;
+
+public interface RankingCallback<T>{
+    void callBack(T ranking);
+}
--- app/src/main/java/com/example/e_study/RankingFragment.java
+++ app/src/main/java/com/example/e_study/RankingFragment.java
@@ -5,13 +5,40 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.e_study.Common.Common;
+import com.example.e_study.Interface.ItemClickListener;
+import com.example.e_study.Interface.RankingCallback;
+import com.example.e_study.Model.Question;
+import com.example.e_study.Model.QuestionScore;
+import com.example.e_study.Model.Ranking;
+import com.example.e_study.ViewHolder.RankingViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class RankingFragment extends Fragment {
     View myFragment;
+
+    RecyclerView rankingList;
+    LinearLayoutManager layoutManager;
+    FirebaseRecyclerAdapter<Ranking, RankingViewHolder> adapter;
+
+    FirebaseDatabase database;
+    DatabaseReference questionScore, rankingTbl;
+
+    int sum = 0;
 
     public static RankingFragment newInstance(){
         RankingFragment rankingFragment = new RankingFragment();
@@ -21,6 +48,11 @@ public class RankingFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        questionScore = database.getReference("Question_Score");
+        rankingTbl = database.getReference("Ranking");
+
     }
 
     @Nullable
@@ -28,6 +60,79 @@ public class RankingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myFragment = inflater.inflate(R.layout.fragment_ranking,container,false);
 
+        //Init view
+        rankingList = (RecyclerView)myFragment.findViewById(R.id.rankingList);
+        layoutManager = new LinearLayoutManager(getActivity());
+        rankingList.setHasFixedSize(true);
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        rankingList.setLayoutManager(layoutManager);
+
+
+
+        updateScore(Common.currentUser.getUserName(), new RankingCallback<Ranking>() {
+            @Override
+            public void callBack(Ranking ranking) {
+                //Update Ranking table
+                rankingTbl.child(ranking.getUserName())
+                        .setValue(ranking);
+                //showRanking(); //Apres upload des donnée , affichage
+
+            }
+        });
+
+        //Set Adapter
+        adapter = new FirebaseRecyclerAdapter<Ranking, RankingViewHolder>(
+                Ranking.class,
+                R.layout.layout_ranking,
+                RankingViewHolder.class,
+                rankingTbl.orderByChild("score")
+        ) {
+            @Override
+            protected void populateViewHolder(RankingViewHolder viewHolder, Ranking model, int position) {
+
+                viewHolder.txt_name.setText(model.getUserName());
+                viewHolder.txt_score.setText(String.valueOf(model.getScore()));
+
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
+            }
+        };
+
+        adapter.notifyDataSetChanged();
+        rankingList.setAdapter(adapter);
+
         return myFragment;
+    }
+
+
+
+    private void updateScore(final String userName, final RankingCallback<Ranking> callback) {
+        questionScore.orderByChild("user").equalTo(userName)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data:dataSnapshot.getChildren())
+                        {
+                            QuestionScore ques = data.getValue(QuestionScore.class);
+                            sum += Integer.parseInt(ques.getScore());
+
+                        }
+                        //Apres résumer de tout les score , nous créons des sum variable
+                        //Car Firebase est asynchrone
+                        Ranking ranking = new Ranking(userName,sum);
+                        callback.callBack(ranking);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
